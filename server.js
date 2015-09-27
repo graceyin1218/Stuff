db5d5cf0225684eef864375e130cf104f649fa2e
graceyin1218 <graceyin1218@gmail.com>
2015-09-27 19:57:44 -0400

    mongo string adding

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var app = express();
+var mongo = require("mongodb");
+var MongoClient = mongo.MongoClient;
+
+
+var photos;
+
+MongoClient.connect("mongodb://localhost:27017/Stuff", function(err, db)
+{
+	if (err) throw err;
+	photos = db.collection("photos");
+});
+
+
 
 app.get('/', function(req, res) {
-  res.send('Hello, world!')
+/*
+  var src = "http://img15.deviantart.net/30a7/i/2012/341/f/f/pichu_vector_by_leek128-d1a1zck.jpg;";
+
+*/
+
+  res.send('Hello, world!');
+//  res.sendFile(src); //("<img src = \" " + src + "\" > ");
+
+	var src = "http://img15.deviantart.net/30a7/i/2012/341/f/f/pichu_vector_by_leek128-d1a1zck.jpg;";
+
+	var count = photos.count();
+	var pretendphoto = {"_id" : src, "count": count};
+
+	//upload photo
+	photos.insert(pretendphoto);
+
 });
 
 
